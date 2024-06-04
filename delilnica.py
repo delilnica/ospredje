3d5bea043852a8adec9dc6a9d8eafb495edcde56
bertronika <nejc@bertoncelj.eu.org>
2024-06-04 14:40:44 +0200

    Odstrani dodajanje iz Flaska

--- delilnica.py
+++ delilnica.py
@@ -57,34 +57,6 @@ def get_fragment(raw_fragment_id: str):
         return render_template("fragment.html", success=success, response=response, status=status,
                                prijavljen=prijavljen(), vzd=vzd())
 
-# ref: nalaganje fragmenta
-@app.route("/add", methods=["POST"])
-def add_fragment():
-    ime      = request.form["ime"]
-    besedilo = request.form["besedilo"]
-    zaseben  = "1" if "zaseben" in request.form else "0"
-
-    if len(ime) < 1 or len(besedilo) < 1:
-        error = "Nekatera polja so ostala neizpolnjena. Vrnite se nazaj in jih dopolnite."
-        return render_template("add.html", success=False, response=error, koda=0)
-
-    r = requests.post(api_url + "/fragment.php", json={
-        "ime": ime,
-        "besedilo": besedilo,
-        "zaseben": zaseben
-        })
-
-    fragment = r.json()
-    response, status = fragment["response"], fragment["status"]
-    success = (r.status_code == 201)
-
-    if success:
-        frag_url = f"/fragment/{response}"
-        return render_template("add.html", success=success, url=frag_url, prijavljen=prijavljen(), vzd=vzd())
-    else:
-        response = r.json()["response"]
-        return render_template("add.html", success=success, response=response, status=status, prijavljen=prijavljen(), vzd=vzd())
-
 @app.route("/register", methods=["GET"])
 def register():
     return render_template("register.html", status=1, prijavljen=prijavljen(), vzd=vzd());
